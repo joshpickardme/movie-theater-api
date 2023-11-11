3ddd611659632500eb3c8f981e28b52117782cf7
joshpickardme <hello@joshpickard.me>
2023-11-11 12:30:51 +0000

    User tests complete

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,7 @@
 const app = require('../src/app')
 const request = require('supertest')
 const {Show, User} = require ('../models/index')
+const { AUTH } = require('sqlite3')
 
 describe('User Testing', () => {
     /*
@@ -16,7 +17,98 @@ describe('User Testing', () => {
     6. Can delete a user
     */
 
-    test('1+1', () => {
-        expect(1+1).toBe(2)
+    test('can get all users', async () => {
+        const allUsers = await request(app).get('/users')
+        expect(allUsers.length).toBe(allUsers.length)
+    })
+
+    test('can get one user', async () => {
+        const oneUser = await request(app).get('/users/5')
+        expect(oneUser.body).toBeTruthy()
+    })
+
+    test('can create a user', async () => {
+        const createUser = await request(app).post('/users').send({
+            'username': 'createdUserForTest',
+            'password': 'createdPasswordForTest'
+        })
+
+        const getUser = createUser.body[createUser.body.length - 1]
+
+        const getUserByPk = await request(app).get(`/users/${getUser.id}`)
+        expect(getUserByPk.body.username).toBe("createdUserForTest")
+    })
+
+    test('cannot create a username if username field is less than three characters or empty', async () => {
+        const createUser = await request(app).post('/users').send({
+            "username": 'a',
+            "password": 'asdoiuwsdiuasdyf'
+        })
+        expect(createUser.body[0].msg).toBe("Invalid value")
+        expect(createUser.body[0].path).toBe("username")
+    })
+
+    test('cannot create a username if password field is less than eight characters or empty', async () => {
+        const createUser = await request(app).post('/users').send({
+            "username": 'abgasduyagsd',
+            "password": 'sir'
+        })
+        expect(createUser.body[0].msg).toBe("Invalid value")
+        expect(createUser.body[0].path).toBe("password")
+    })
+
+    test('cannot create a username if both input fields are empty', async () => {
+        const createUser = await request(app).post('/users').send({
+            "username": '',
+            "password": ''
+        })
+        expect(createUser.body.length).toBe(4)
+    })
+
+    test('can get all shows watched by a user', async () => {
+        const createUser = await request(app).post('/users').send({
+            "username": 'showsWatched',
+            "password": 'showsWatched'
+        })
+
+        
+
+        const getUser = createUser.body[createUser.body.length - 1]
+
+        // PUT Some shows
+        await request(app).put(`/users/${getUser.id}/shows/10`)
+        await request(app).put(`/users/${getUser.id}/shows/11`)
+        await request(app).put(`/users/${getUser.id}/shows/12`)
+
+        const allShowsWatched = await request(app).get(`/users/${getUser.id}/shows`)
+
+        expect(allShowsWatched.body.length).toBe(3)
+    })
+
+    test('can add a show if a user has watched it', async () => {
+        const createUser = await request(app).post('/users').send({
+            "username": 'showsWatched',
+            "password": 'showsWatched'
+        })
+
+        const getUser = createUser.body[createUser.body.length - 1]
+        await request(app).put(`/users/${getUser.id}/shows/10`)
+        const allShowsWatched = await request(app).get(`/users/${getUser.id}/shows`)
+
+        expect(allShowsWatched.body[0].title).toBe("Test Show")
+    })
+
+    test('can delete a user', async () => {
+        const createUser = await request(app).post('/users').send({
+            "username": 'toBeDeleted',
+            'password': "toBeDeleted"
+        })
+
+        const getUser = createUser.body[createUser.body.length - 1];
+
+        await request(app).delete(`/users/${getUser.id}`)
+
+        const getDeletedUser = await request(app).get(`/users${getUser.id}`)
+        expect(getDeletedUser.body.title).toBeFalsy()
     })
 })
